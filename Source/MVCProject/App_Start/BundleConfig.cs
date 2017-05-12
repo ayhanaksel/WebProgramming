149a3628b112c881b9426341356e68c8dd40f049
Ayhan AKSEL <akselayhan@gmail.com>
2017-05-12 15:56:29 +0300

    Source

---
+++ Source/MVCProject/App_Start/BundleConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace MVCProject.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles (BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Admin/Styles")
+                .Include("~/Content/Styles/bootstrap.css")
+                .Include("~/Content/Styles/Admin.css")
+                );
+
+            bundles.Add(new StyleBundle("~/Styles")
+                .Include("~/Content/Styles/bootstrap.css")
+                .Include("~/Content/Styles/Site.css")
+                );
+
+            bundles.Add(new ScriptBundle("~/Content/Scripts")
+                .Include("~/Scripts/jquery-1.8.0.js")
+                .Include("~/Scripts/jquery.validate.js")
+                .Include("~/Scripts/jquery.validate.unobtrusive.js")
+                .Include("~/Scripts/bootstrap.js")
+                );
+
+            bundles.Add(new ScriptBundle("~/Content/Scripts")
+                .Include("~/Scripts/jquery-1.8.0.js")
+                .Include("~/Scripts/jquery.validate.js")
+                .Include("~/Scripts/jquery.validate.unobtrusive.js")
+                .Include("~/Scripts/bootstrap.js")
+                );
+        }
+    }
+}
---
+++ Source/MVCProject/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using MVCProject.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MVCProject.Controllers
+{
+    public class AuthController : Controller
+    {
+        public ActionResult login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult login(AuthLogin form)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(form);
+            }
+            FormsAuthentication.SetAuthCookie(form.UserName, true);
+
+            return Content("Hi " + form.UserName);
+        }
+    }
+}
